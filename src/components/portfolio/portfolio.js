afff9c26ede8e1fa06da57e4fb08b985a6ca8f27
bilalfareed83 <74311496+cssrgit@users.noreply.github.com>
2022-03-30 17:53:24 +0500

    update portfolio component

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./portfolio.css";
 import image1 from "../../images/portfolio1.jpg";
 import image2 from "../../images/portfolio2.jpg";
@@ -11,6 +11,28 @@ import image8 from "../../images/portfolio8.jpg";
 import { Link } from "react-router-dom";
 
 export const Portfolio = () => {
+  const [youtube, setYoutube] = useState(true);
+  const [website, setWebsite] = useState(false);
+  const [facebook, setFacebook] = useState(false);
+
+  const data = [
+    { images: image1, val: "project 1", link: "#", ref: youtube },
+    { images: image7, val: "project 7", link: "#", ref: youtube },
+    { images: image8, val: "project 8", link: "#", ref: youtube },
+    { images: image2, val: "project 2", link: "#", ref: website },
+    { images: image3, val: "project 3", link: "#", ref: website },
+    { images: image4, val: "project 4", link: "#", ref: facebook },
+    { images: image4, val: "project 5", link: "#", ref: facebook },
+    { images: image6, val: "project 6", link: "#", ref: facebook },
+  ];
+
+  const clickedAnchorBotton = (e) => {
+    let btn_num = e.target.dataset.btnNum;
+    let img_active = `.p-btn--${btn_num}`;
+    console.log(img_active);
+    // .p-btn--1
+  };
+
   return (
     <section className="section section-portfolio" id="portfolio-section">
       <div className="container">
@@ -20,27 +42,51 @@ export const Portfolio = () => {
           esse sit.{" "}
         </p>
       </div>
-      <div className="p-btns">
-        <div className="btn p-btn" data-btn-num="1">
+      <div className="p-btns" onClick={clickedAnchorBotton}>
+        <div
+          className={website ? "btn p-btn p-btn-active" : "btn p-btn"}
+          data-btn-num="1"
+          onClick={(e) => {
+            return setWebsite(!website), setFacebook(false), setYoutube(false);
+          }}
+        >
           Website
         </div>
-        <div className="btn p-btn" data-btn-num="2">
+        <div
+          className={youtube ? "btn p-btn p-btn-active" : "btn p-btn"}
+          data-btn-num="2"
+          onClick={(e) => {
+            return setWebsite(false), setFacebook(false), setYoutube(!youtube);
+          }}
+        >
           Youtube
         </div>
-        <div className="btn p-btn" data-btn-num="3">
+        <div
+          className={facebook ? "btn p-btn p-btn-active" : "btn p-btn"}
+          data-btn-num="3"
+          onClick={(e) => {
+            return setWebsite(false), setFacebook(!facebook), setYoutube(false);
+          }}
+        >
           Facbook
         </div>
       </div>
       <div className="container grid grid-three-column portfolio-images">
-        <div className="img-overlay p-btn--1">
-          <img src={image1} alt="portfolio" />
-          <div className="overlay">
-            <Link to="#" className="common-heading">
-              Project 1
-            </Link>
-          </div>
-        </div>
-        <div className="img-overlay p-btn--1">
+        {data
+          .filter((item) => item.ref)
+          .map((item, index) => {
+            return (
+              <div className="img-overlay p-btn--1" key={index}>
+                <img src={item.images} alt={item.val} />
+                <div className="overlay">
+                  <Link to={item.link} className="common-heading">
+                    {item.val}
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
+        {/* <div className="img-overlay p-btn--1">
           <img src={image2} alt="portfolio" />
           <div className="overlay">
             <Link to="#" className="common-heading">
@@ -79,7 +125,7 @@ export const Portfolio = () => {
               Project 6
             </Link>
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
